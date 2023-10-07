d22eef23861369b0185dff7fa5577d8ac6d0fdec
Enjamamul Hoque <enjamamul.official.ac@gmail.com>
2023-10-07 23:55:48 +0530

    Initial commit

---
+++ app/src/main/java/com/enjamamulhoque/bottomnavigationfeature/MainActivity.java
@@ -0,0 +1,58 @@
+package com.enjamamulhoque.bottomnavigationfeature;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+
+        HomeFragment homeFragment = new HomeFragment();
+        NotificationFragment notificationFragment = new NotificationFragment();
+        AccountFragment accountFragment = new AccountFragment();
+
+        // load home fragment by default
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view, homeFragment).commit();
+                        return true;
+
+                    case R.id.notification:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view, notificationFragment).commit();
+                        return true;
+
+                    case R.id.account:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view, accountFragment).commit();
+                        return true;
+
+                    default:
+                        return true;
+
+                }
+
+            }
+        });
+    }
+}
